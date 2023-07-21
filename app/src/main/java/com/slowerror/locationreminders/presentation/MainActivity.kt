56b64d51837385318894c5ff40bc034f10718e4a
Nikolay Zetkin <nszetkin76@gmail.com>
2023-07-21 11:57:29 +0300

    сверстал макет для AddReminderFragment.kt, добавил toolbar

--- app/src/main/java/com/slowerror/locationreminders/presentation/MainActivity.kt
+++ app/src/main/java/com/slowerror/locationreminders/presentation/MainActivity.kt
@@ -2,15 +2,33 @@ package com.slowerror.locationreminders.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.slowerror.locationreminders.R
 import com.slowerror.locationreminders.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+        setupToolbar()
     }
+
+    private fun setupToolbar() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.navContainer) as NavHostFragment
+        navController = navHost.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+    }
+
 }
--- app/src/main/java/com/slowerror/locationreminders/presentation/ui/reminder_list/ReminderListFragment.kt
+++ app/src/main/java/com/slowerror/locationreminders/presentation/ui/reminder_list/ReminderListFragment.kt
@@ -3,13 +3,19 @@ package com.slowerror.locationreminders.presentation.ui.reminder_list
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.slowerror.locationreminders.R
 import com.slowerror.locationreminders.databinding.FragmentReminderListBinding
+import com.slowerror.locationreminders.presentation.MainActivity
 import com.slowerror.locationreminders.presentation.ui.reminder_list.adapter.ReminderAdapter
 import com.slowerror.locationreminders.presentation.ui.reminder_list.listener.ReminderClickListener
 
@@ -32,7 +38,6 @@ class ReminderListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initAdapter()
-
         viewModel.reminderList.observe(viewLifecycleOwner) {
             reminderAdapter.submitList(it)
         }
@@ -42,8 +47,8 @@ class ReminderListFragment : Fragment() {
     }
 
     override fun onDestroyView() {
-        _binding = null
         super.onDestroyView()
+        _binding = null
     }
 
     private fun initAdapter() {
